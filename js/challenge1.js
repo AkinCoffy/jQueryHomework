2b165ee665dcaf9935f26264a0ad03323ef70500
Akin <33817966+AkinCoffy@users.noreply.github.com>
2019-11-26 23:30:41 -0500

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,10 @@
+$(subscribe).click(function(){
+	if (this.checked){
+		console.log("selected"); 
+		$("#emailField").css("display","block");
+	}
+	else{
+		console.log("Not selected");
+		$("#emailField").css("display","none");
+	}
+});
